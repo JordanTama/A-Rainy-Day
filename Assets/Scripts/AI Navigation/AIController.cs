213f3a32994b6717daea4c9a1782825e7d338b76
Dale Grant <dale.t.grant@student.uts.edu.au>
2020-08-30 21:08:57 +1000

    [Bugfix] Incorporating Navmesh AI into level

--- Assets/Scripts/AI Navigation/AIController.cs
+++ Assets/Scripts/AI Navigation/AIController.cs
@@ -7,10 +7,30 @@ public class AIController : MonoBehaviour
 {
     [SerializeField] private AIManager manager;
     [SerializeField] private bool playOnAwake;
+    private GameLoopManager _gameLoopManager;
 
     private void Awake()
     {
         manager.Initialize();
         if (playOnAwake) manager.Play();
+    }
+
+    private void Start()
+    {
+        _gameLoopManager = ServiceLocator.Current.Get<GameLoopManager>();
+        _gameLoopManager.OnPreparation += StopSpawning;
+        _gameLoopManager.OnExecution += StartSpawning;
+        _gameLoopManager.OnComplete += StopSpawning;
+    }
+
+    public void StartSpawning()
+    {
+        manager.Play();
+    }
+
+    public void StopSpawning()
+    {
+        manager.Pause();
+        manager.ClearAgents();
     }
 }
