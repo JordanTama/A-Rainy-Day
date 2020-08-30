4de15475f390bb6ab4f2d8bb58e105760cfae4d9
Dale Grant <dale.t.grant@student.uts.edu.au>
2020-08-30 22:58:57 +1000

    [US05] End goal is distinct for player to focus on
    #7 As a player, I want the end goal of the level to be visually distinct so that I can easily identify the primary goal of each level

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void Quit()
+    {
+        if (UnityEngine.Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
