7aad3ec05ea619b60ef185b94e68ac7f0e043f69
Olly Rybak <ollyrybak@gmail.com>
2020-08-30 18:33:27 +1000

    [US73] Fixed up the in-game feedback form
    #75

--- Assets/Scripts/FeedbackController.cs
+++ Assets/Scripts/FeedbackController.cs
@@ -4,27 +4,52 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class FeedbackController : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI feedbackOpenBtnText;
     [SerializeField] private TMP_InputField feedbackText;
     [SerializeField] private RectTransform feedbackRect;
+    [SerializeField] private Button sendButton;
+    [SerializeField] private CanvasGroup responsePanel;
+    [SerializeField] private TextMeshProUGUI responseText;
 
     private bool isOpen = false;
 
     public void ToggleFeedback()
     {
         isOpen = !isOpen;
-        if(!isOpen)
+        if (isOpen)
+        {
             feedbackRect.DOAnchorPosY(0, 0.25f);
+            feedbackOpenBtnText.text = "vv Feedback vv";
+        }
         else
-            feedbackRect.DOAnchorPosY(-356, 0.25f);
+        {
+            Close();
+        }
     }
 
     [ContextMenu("Send Feedback")]
     public void SendFeedback()
     {
         StartCoroutine("SendForm");
+        sendButton.interactable = false;
+        responsePanel.DOFade(1, 0.5f);
+        responsePanel.blocksRaycasts = true;
+        responseText.text = "Sending...";
+    }
+
+    public void Close()
+    {
+        feedbackRect.DOAnchorPosY(-356, 0.25f);
+        responsePanel.blocksRaycasts = false;
+        responsePanel.alpha = 0;
+        sendButton.interactable = true;
+        feedbackText.text = "";
+        isOpen = false;
+        feedbackOpenBtnText.text = "^^ Feedback ^^";
     }
 
     IEnumerator SendForm()
@@ -38,11 +63,16 @@ public class FeedbackController : MonoBehaviour
             if(w.isNetworkError || w.isHttpError)
             {
                 Debug.LogError(w.error);
+                responseText.text = "Something went wrong. :(";
             }
             else
             {
                 Debug.Log("Feedback sent.");
+                responseText.text = "Thank you! :)";
             }
         }
+
+        yield return new WaitForSecondsRealtime(1);
+        Close();
     }
 }
