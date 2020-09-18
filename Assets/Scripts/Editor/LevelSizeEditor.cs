8bb2352d3074e533e37c8af60233a269f077bb3d
Dale Grant <dale.t.grant@student.uts.edu.au>
2020-09-18 22:06:01 +1000

    [US 96] Size of the blockers can be adjusted in inspector
    #100

---
+++ Assets/Scripts/Editor/LevelSizeEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelSize))]
+public class LevelSizeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelSize levelSize = (LevelSize) target;
+        if(GUILayout.Button("Resize Level"))
+        {
+            levelSize.SetLevelSize();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelSize : MonoBehaviour
+{
+    public int NumberOfTilesWide;
+    public int NumberOfTilesDeep;
+
+    private int _defaultTileWidth = 3;
+    private int _defaultTileDepth = 3;
+    private float _blockSize = 5f;
+    
+
+    public void SetLevelSize()
+    {
+        transform.position = new Vector3((NumberOfTilesWide-_defaultTileWidth )*_blockSize/6,0,(_defaultTileDepth - NumberOfTilesDeep)*_blockSize/2);
+        transform.localScale = new Vector3((float)NumberOfTilesWide/(float)_defaultTileWidth,1f,(float)NumberOfTilesDeep/(float)_defaultTileDepth);
+    }
+
+}
