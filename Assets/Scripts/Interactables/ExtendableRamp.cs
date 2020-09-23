89d20bb904f623f96823887e565fb126bdaa12e5
Dale Grant <dale.t.grant@student.uts.edu.au>
2020-09-23 17:54:40 +1000

    [US 108] Extendable ramps now functional
    #118

---
+++ Assets/Scripts/Interactables/ExtendableRamp.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ExtendableRamp : InteractableReceiver
+{
+    private Vector3 _retractPosition;
+    private Vector3 _extendPosition;
+    [SerializeField]private NavMeshObstacle _obstacle;
+    [SerializeField] private Transform _visualsTransform;
+    private bool isExtended;
+    public bool bStartExtended;
+
+    protected new void Awake()
+    {
+        base.Awake();
+        
+        InitializePositions();
+        
+    }
+    protected new void Start()
+    {
+        base.Start();
+        ResetState();
+
+    }
+
+    protected override void ChangeState()
+    {
+        base.ChangeState();
+        if(isExtended) RetractRamp();
+        else ExtendRamp();
+    }
+
+    private void InitializePositions()
+    {
+        if (bStartExtended)
+        {
+            
+            _extendPosition = _visualsTransform.localPosition;
+            _retractPosition = _visualsTransform.localPosition - _visualsTransform.localScale.z * Vector3.forward;
+        }
+        else
+        {
+            _extendPosition = _visualsTransform.localPosition + _visualsTransform.localScale.z * Vector3.forward;
+            _retractPosition = _visualsTransform.localPosition;
+        }
+        
+        
+    }
+
+    protected override void ResetState()
+    {
+        base.ResetState();
+        if(bStartExtended) ExtendRamp();
+        else RetractRamp();
+    }
+
+    private void ExtendRamp()
+    {
+        isExtended = true;
+        if (_obstacle)
+        {
+            _obstacle.carving = !isExtended;
+            _obstacle.enabled = !isExtended;
+        }
+        _visualsTransform.localPosition = _extendPosition;
+    }
+
+    private void RetractRamp()
+    {
+        isExtended = false;
+        if (_obstacle)
+        {
+            _obstacle.carving = !isExtended;
+            _obstacle.enabled = !isExtended;
+        }
+        _visualsTransform.localPosition = _retractPosition;
+    }
+}
