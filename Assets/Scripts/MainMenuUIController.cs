93b403eaea0a71c12d1e3b888554d3e5c2ad09dc
Olly Rybak <ollyrybak@gmail.com>
2020-09-27 16:31:12 +1000

    [US 18, 20, 22] Added first pass of main menu

---
+++ Assets/Scripts/MainMenuUIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MainMenuUIController : MonoBehaviour
+{
+
+    [SerializeField] private float _tweenSpeed;
+    [SerializeField] private RectTransform _mainMenu;
+    [SerializeField] private RectTransform _chapterSelect;
+
+    private void Start()
+    {
+        _chapterSelect.anchoredPosition = new Vector2(1920, 0);
+    }
+
+    public void LoadLevel(string LevelName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(LevelName);
+    }
+
+    public void FromMainToChapterSelect()
+    {
+        _mainMenu.DOAnchorPosX(-1920f, _tweenSpeed);
+        _chapterSelect.DOAnchorPosX(0f, _tweenSpeed);
+    }
+
+    public void FromChapterSelectToMain()
+    {
+        _chapterSelect.DOAnchorPosX(1920f, _tweenSpeed);
+        _mainMenu.DOAnchorPosX(0f, _tweenSpeed);
+    }
+}
